de303e2e8c6685b2270e783a4134ad8d561550e5
jstudenski <jstudenski@gmail.com>
2018-04-08 19:08:16 -0500

    clean up Scoreboard

--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -2,28 +2,25 @@ import React from "react";
 import "./Scoreboard.css";
 
 class Scoreboard extends React.Component {
-    state = {
-        score: 0
-    };
+  state = {
+    score: 0
+  };
 
-    handleIncrement = () => {
-        // We always use the setState method to update a component's state
-        this.setState({ score: this.state.score + 1 });
-      };
+  handleIncrement = () => {
+    // We always use the setState method to update a component's state
+    this.setState({ score: this.state.score + 1 });
+  };
 
-    render() {
-        return (
-          <div className="panel panel-primary">
-            <div className="panel-heading">Click Counter Component</div>
-            <div className="panel-body text-center">
-              <p>Click Count -- {this.state.score}</p>
-              <button className="btn btn-primary" onClick={this.handleIncrement}>
-                Increment
-              </button>
-            </div>
-          </div>
-        );
-      }
+  render() {
+    return (
+      <div>
+        <p>Click Count -- {this.state.score}</p>,
+        <button className="btn btn-primary" onClick={this.handleIncrement}>
+          Increment
+        </button>
+      </div>
+    );
+  }
 
 }
 
