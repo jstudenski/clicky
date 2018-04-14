7f1df2b20eacd1fd5810debfa5a8f4f9ec06550f
jstudenski <jstudenski@gmail.com>
2018-04-14 09:30:03 -0500

    clean

--- src/App.js
+++ src/App.js
@@ -4,22 +4,19 @@ import './App.css';
 import Wrapper from './components/Wrapper';
 import Card from './components/Card';
 import Scoreboard from './components/Scoreboard';
-import children from './friends.json';
+import cards from './cards.json';
 
 
 const shuffleArray = arr => arr.sort(() => Math.random() - 0.5);
 
 class App extends Component {
   state = {
-    children,
+    cards,
     guessed: [],
     score: 0,
     topScore: 0,
   };
-  // const friends = this.state.friends.filter(friend => friend.id !== id);
-  // const guessed = this.state.guessed += 1
-  // // this.state.friends.guessed = true
-  // this.setState({ friends, guessed });
+
   handleClick = (id) => {
     if (this.state.guessed.indexOf(id) === -1) {
       this.state.guessed.push(id);
@@ -33,33 +30,11 @@ class App extends Component {
       this.setState({
         score: 0,
         guessed: [],
-        // reset score and empty guessed array
       });
     }
-
-    // console.log(this.state.guessed);
-    shuffleArray(this.state.children);
-
-    // // push to array if it hasn't been guessed
-    // this.state.guessed.indexOf(id) === -1 && (this.state.guessed.push(id)),
-    // // push to array if it hasn't been guessed
-    // this.state.guessed.indexOf(id) === -1 && (this.setState({ score: this.state.score + 1 })),
-
-    // this.setState({ score: this.state.score + 1 }),
-    // this.setState({ topScore: this.state.topScore + 2 }),
-
-    // console.log(),
-    // console.log(),
-    // console.log(this.state.guessed);
+    shuffleArray(this.state.cards);
   };
 
-  // removeFriend = id => {
-  //   // Filter this.state.friends for friends with an id not equal to the id being removed
-  //   // const friends = this.state.friends.filter(friend => friend.id !== id);
-
-  //   // // Set this.state.friends equal to the new friends array
-  //   // this.setState({ friends });
-  // }
   render() {
     return (
       <div>
@@ -75,7 +50,7 @@ class App extends Component {
           />
         </header>
         <Wrapper>
-          {this.state.children.map(friend => (
+          {this.state.cards.map(friend => (
             <Card
               handleClick={this.handleClick}
               key={friend.id}
